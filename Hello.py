b4297447dc15c250e94cbe218d866ac81011dccf
zhange <dka_zhange@163.com>
2016-10-04 13:03:39 +0800

    hello world

---
+++ Hello.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+name = input("Please input your name:")
+print (type(name))
+
+for x in range(10):
+	print ("hello", x, "hello")
+
+if name == "zhange":
+	print("welcome")
+	pass
+
+while name == "xiaosi":
+	print("I am while")
+	pass
