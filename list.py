96271965edca542bc5e7d66ee4d988dbb4055393
zhange <dka_zhange@163.com>
2016-10-04 13:16:09 +0800

    列表应用

---
+++ list.py
@@ -0,0 +1,18 @@
+list = ['Tom', 'Bob', 'Tracy']
+#打印列表
+print(list)
+#打印列表长度
+print(len(list))
+#遍历列表
+for x in list:
+	print(x)
+	pass
+#列表末尾追加
+list.append('zhange')
+print(list)
+#列表删除末尾
+list.pop()
+print(list)
+#删除指定位置
+list.pop(1)
+print(list)
